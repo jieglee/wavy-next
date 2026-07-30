f33df8e129ef3d69c28345467778066044e39329
jieffrey <kyooon08@gmail.com>
2026-07-30 21:23:44 +0700

    HIT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/landing/navbar";
 import Hero from "@/components/landing/hero";
 import Featured from "@/components/landing/featured";
+import HowItWorks from "@/components/landing/how-it-works";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <Featured />
+        <HowItWorks />
       </main>
     </>
   );
