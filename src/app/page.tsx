06319930433bb13cc9fa2026d8590aa19f75b94e
jieffrey <kyooon08@gmail.com>
2026-07-30 21:21:40 +0700

    featured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/landing/navbar";
 import Hero from "@/components/landing/hero";
+import Featured from "@/components/landing/featured";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Navbar />
       <main className="flex-1">
         <Hero />
+        <Featured />
       </main>
     </>
   );
