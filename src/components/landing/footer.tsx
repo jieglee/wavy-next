70c94730a8297db008f447c9b885a10da2b318f6
jieglee <callmezie23@gmail.com>
2026-07-31 19:01:04 +0700

    footer-alfa

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -34,7 +34,7 @@ const paymentLogos: Record<string, { src: string; width: number; height: number 
   ShopeePay: { src: "/images/footer/shopeepay.png", width: 842, height: 370 },
   Visa: { src: "/images/footer/Visa_Inc._logo_(2021–present).svg.webp", width: 3840, height: 1247 },
   Mastercard: { src: "/images/footer/Mastercard-logo.svg.webp", width: 3840, height: 2983 },
-  Alfamart: { src: "/images/footer/alfamart.png", width: 998, height: 370 },
+  Alfamart: { src: "/images/footer/ALFAMART_LOGO_BARU.png", width: 998, height: 370 },
   Indomaret: { src: "/images/footer/Logo_Indomaret.png", width: 1280, height: 448 },
 }
 
