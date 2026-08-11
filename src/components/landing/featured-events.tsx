758d852b988e4f40062453b4ba4d26fb3361a5da
jieglee <callmezie23@gmail.com>
2026-08-11 17:41:34 +0700

    featured

--- src/components/landing/featured-events.tsx
+++ src/components/landing/featured-events.tsx
@@ -83,8 +83,8 @@ export default function FeaturedEvents() {
                 <div className="mb-6 flex items-center gap-2">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        width="22"
-                        height="22"
+                        width="30"
+                        height="30"
                         viewBox="0 0 24 24"
                         className="animate-megaphone text-wavy-accent"
                     >
