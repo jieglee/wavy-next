88f6d3e94971784b0e84ba76e466bfdfda50db0b
jieglee <callmezie23@gmail.com>
2026-09-07 14:56:56 +0700

    negara

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-    locales: ["id", "en"],
+    locales: ["id", "en", "ko"],
     defaultLocale: "id",
 });
