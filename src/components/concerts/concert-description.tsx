baf676b92cd85a7e230efae57c3b12fdfd7be5fa
jieglee <callmezie23@gmail.com>
2026-09-09 09:43:05 +0700

    feat: add detail description

--- src/components/concerts/concert-description.tsx
+++ src/components/concerts/concert-description.tsx
@@ -2,8 +2,8 @@ import { forwardRef } from "react";
 import { sanitizeHtml } from "@/lib/sanitize";
 
 const ConcertDescription = forwardRef<HTMLDivElement, { description: string }>(({ description }, ref) => (
-  <section ref={ref} id="sec-desc" className="scroll-mt-[140px] rounded-2xl bg-white p-5 sm:p-6">
-    <div className="tnc-html mt-3 whitespace-pre-line text-[14px] leading-7 text-[#374151]" dangerouslySetInnerHTML={{ __html: sanitizeHtml(description || "") }} />
+  <section ref={ref} id="sec-desc" className="scroll-mt-[140px]">
+    <div className="tnc-html whitespace-pre-line text-[14px] leading-7 text-[#4B5563]" dangerouslySetInnerHTML={{ __html: sanitizeHtml(description || "") }} />
   </section>
 ));
 ConcertDescription.displayName = "ConcertDescription";
