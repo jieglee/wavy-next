5b6d9eac1911daab5805d01c68a8d3c61bb3b8b9
jieglee <callmezie23@gmail.com>
2026-09-16 09:55:39 +0700

    fix(hero): rounded-top poster - rounded-t-xl

--- src/components/concerts/concert-hero.tsx
+++ src/components/concerts/concert-hero.tsx
@@ -96,7 +96,7 @@ export default function ConcertHero({ concert }: { concert: ConcertDetail }) {
             <img
               src={banner}
               alt={concert.title}
-              className="block h-full w-full object-contain object-right-bottom"
+              className="block h-full w-full rounded-b-none rounded-t-xl object-contain object-right-bottom"
             />
           </div>
         </div>
