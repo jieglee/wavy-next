7511551bd690249d5a1a6e460d52585624f17e2f
jieglee <callmezie23@gmail.com>
2026-09-16 09:56:27 +0700

    fix(hero): poster top rounded with overflow-hidden container

--- src/components/concerts/concert-hero.tsx
+++ src/components/concerts/concert-hero.tsx
@@ -92,11 +92,11 @@ export default function ConcertHero({ concert }: { concert: ConcertDetail }) {
           </div>
           </div>
 
-          <div className="relative z-10 hidden h-[460px] w-[520px] translate-y-[-160px] items-end justify-end justify-self-end self-end pr-2 lg:flex">
+          <div className="relative z-10 hidden h-[460px] w-[520px] translate-y-[-160px] items-end justify-end justify-self-end self-end overflow-hidden rounded-t-xl pr-2 lg:flex">
             <img
               src={banner}
               alt={concert.title}
-              className="block h-full w-full rounded-b-none rounded-t-xl object-contain object-right-bottom"
+              className="block h-full w-full object-contain object-right-bottom"
             />
           </div>
         </div>
